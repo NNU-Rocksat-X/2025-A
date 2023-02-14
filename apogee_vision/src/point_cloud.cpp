ce75ea79cbdba35765cfd88c5d94d7a482aefaf5
aborger <borger.aaron@gmail.com>
2023-02-13 22:00:10 -0800

    Added color filter and speeding up tracking

--- apogee_vision/src/point_cloud.cpp
+++ apogee_vision/src/point_cloud.cpp
@@ -4,6 +4,8 @@
 #include <eigen3/Eigen/Dense>
 #include <pcl/point_cloud.h>
 #include <pcl_conversions/pcl_conversions.h>
+#include <pcl/filters/voxel_grid.h>
+#include <daedalus_msgs/RGBDFrame.h>
 #include <math.h>
 
 #define FOVY (45)
@@ -24,6 +26,20 @@ struct CamProperties {
 
 ros::Publisher pc_pub;
 int seq_ = 0;
+
+// Access according to 
+// http://docs.ros.org/en/melodic/api/std_msgs/html/msg/MultiArrayLayout.html
+class ColorFrame
+{
+    public:
+    std_msgs::UInt8MultiArray* arr;
+    ColorFrame(std_msgs::UInt8MultiArray* _arr) : arr{_arr} {}
+
+    uint8_t access(int i, int j, int k)
+    {
+        return arr->data[arr->layout.dim[1].stride*i + arr->layout.dim[2].stride*j + k];
+    }
+};
 
 
 void initialize_cam(CamProperties &cam, int height, int width)
@@ -46,13 +62,14 @@ void depthCB(const std_msgs::Float32MultiArray::ConstPtr& msg)
         initialize_cam(cam_, msg->layout.dim[0].size, msg->layout.dim[1].size);
     }
 
-    std::vector<float> data = msg->data;
-    Frame frame(data.data(), cam_.height, cam_.width); // TODO: allocate this once to prevent memory holes
-    float min = frame.minCoeff();
-    float max = frame.maxCoeff();
+    std::vector<float> d_data = msg->data;
+    Frame d_frame(d_data.data(), cam_.height, cam_.width);
+    
+    float min = d_frame.minCoeff();
+    float max = d_frame.maxCoeff();
 
     // Scale between 0 and 1
-    frame = (1 - (frame.array() - min) / (max - min)).matrix();
+    d_frame = (1 - (d_frame.array() - min) / (max - min)).matrix();
 
     
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloudPtr(new pcl::PointCloud<pcl::PointXYZ>);
@@ -61,7 +78,7 @@ void depthCB(const std_msgs::Float32MultiArray::ConstPtr& msg)
         for (int r = 0; r < cam_.height; r++)
         {
             pcl::PointXYZ p;
-            p.z = frame(r, c);
+            p.z = d_frame(r, c);
             p.x = (c - cam_.posX) * p.z / cam_.focalX;
             p.y = (r - cam_.posY) * p.z / cam_.focalY;
             cloudPtr->points.push_back(p);
@@ -69,12 +86,20 @@ void depthCB(const std_msgs::Float32MultiArray::ConstPtr& msg)
     }
 
     // Convert PointXYZ to pcl::PointCloud2
-    pcl::PCLPointCloud2 pcl_pc2;
-    pcl::toPCLPointCloud2(*cloudPtr, pcl_pc2);
+    pcl::PCLPointCloud2::Ptr pcl_pc2(new pcl::PCLPointCloud2);
+    pcl::toPCLPointCloud2(*cloudPtr, *pcl_pc2);
+
+    // Reduce number of points for performance
+    pcl::PCLPointCloud2::Ptr filtered_cloud(new pcl::PCLPointCloud2());
+    pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+    sor.setInputCloud(pcl_pc2);
+    sor.setLeafSize(0.001f, 0.001f, 0.001f);
+    sor.filter(*filtered_cloud);
+
 
     // Convert pcl::PointCloud2 to sensor_msgs::PointCloud2
     sensor_msgs::PointCloud2 pc2;
-    pcl_conversions::fromPCL(pcl_pc2, pc2);
+    pcl_conversions::fromPCL(*filtered_cloud, pc2);
 
     pc2.header.seq = seq_;
     pc2.header.frame_id = "world";
@@ -85,13 +110,15 @@ void depthCB(const std_msgs::Float32MultiArray::ConstPtr& msg)
 
 }
 
+
+
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "point_cloud");
     ros::NodeHandle nh;
     cam_.initialized = false;
 
-    ros::Subscriber depth_sub = nh.subscribe("/depth", 1, &depthCB);
+    ros::Subscriber frame_sub = nh.subscribe("/depth", 1, &depthCB);
     pc_pub = nh.advertise<sensor_msgs::PointCloud2>("/point_cloud", 10);
 
     ros::spin();
--- apogee_vision/src/trajectory_prediction/trajectory_predictor.cpp
+++ apogee_vision/src/trajectory_prediction/trajectory_predictor.cpp
@@ -44,223 +44,221 @@ geometry_msgs::Quaternion quat_to_msg(const Eigen::Quaternionf q)
 // Then there is a predictor which contains the ros services
 class Predictor {
     private:
-        PositionFilter* pos_filter;
-        OrientationPredictor* ori_predictor;
+    PositionFilter* pos_filter;
+    OrientationPredictor* ori_predictor;
 
-        Eigen::Vector3f measured_position;
-        Eigen::Vector4f measured_orientation;
-        ros::Time init_time;
+    Eigen::Vector3f measured_position;
+    Eigen::Vector4f measured_orientation;
+    ros::Time init_time;
 
-        bool position_recvd = false;
-        bool has_converged = false;
+    bool position_recvd = false;
+    bool has_converged = false;
 
-        // Ros comm
-        ros::NodeHandle* nh;
-        ros::Subscriber sub;
-        ros::Publisher or_pub;
-        ros::ServiceServer convergenceService;
-        ros::ServiceServer predictService;
-        ros::ServiceServer observeService;
+    // Ros comm
+    ros::NodeHandle* nh;
+    ros::Subscriber sub;
+    ros::Publisher or_pub;
+    ros::ServiceServer convergenceService;
+    ros::ServiceServer predictService;
+    ros::ServiceServer observeService;
 
 
     public:
-        Predictor(ros::NodeHandle* nh_)
+    Predictor(ros::NodeHandle* nh_)
+    {
+        nh = nh_;
+
+        sub = nh->subscribe("mujoco/object_pose", 1, &Predictor::pose_cb, this);
+        or_pub = nh->advertise<daedalus_msgs::OrientationState>("/orientation_state", 10);
+        convergenceService = nh->advertiseService("trajectory_predictor/ready", &Predictor::prediction_ready, this);
+        predictService = nh->advertiseService("trajectory_predictor/predict", &Predictor::predict, this);
+        observeService = nh->advertiseService("trajectory_predictor/observe", &Predictor::observe, this);
+
+    }
+
+    void pose_cb(const geometry_msgs::Pose::ConstPtr& msg)
+    {
+        measured_position = point_to_v3f(msg->position);
+        measured_orientation = quat_to_v4f(msg->orientation);
+
+        if (!position_recvd)
         {
-            nh = nh_;
-
-            sub = nh->subscribe("mujoco/object_pose", 1, &Predictor::pose_cb, this);
-            or_pub = nh->advertise<daedalus_msgs::OrientationState>("/orientation_state", 10);
-            convergenceService = nh->advertiseService("trajectory_predictor/ready", &Predictor::prediction_ready, this);
-            predictService = nh->advertiseService("trajectory_predictor/predict", &Predictor::predict, this);
-            observeService = nh->advertiseService("trajectory_predictor/observe", &Predictor::observe, this);
-
+            position_recvd = true;
+            init_time = ros::Time::now();
         }
 
-        void pose_cb(const geometry_msgs::Pose::ConstPtr& msg)
+    }
+
+    // Currently only works for position
+    bool observe(daedalus_msgs::ObjectObservation::Request &req,
+                    daedalus_msgs::ObjectObservation::Response &res)
+    {
+        Position::State state;
+        pos_filter->get_state(state);
+
+        Eigen::Quaternionf orientation;
+        Eigen::Vector3f angular_velocity;
+        ori_predictor->get_state(orientation, angular_velocity);
+
+        geometry_msgs::Pose pmsg;
+        // Position
+        pmsg.position.x = state.position[0];
+        pmsg.position.y = state.position[1];
+        pmsg.position.z = state.position[2];
+
+        // Orientation
+        pmsg.orientation.w = orientation.w();
+        pmsg.orientation.x = orientation.vec()[0];
+        pmsg.orientation.y = orientation.vec()[1];
+        pmsg.orientation.z = orientation.vec()[2];
+
+        geometry_msgs::Twist vmsg;
+        // Velocity
+        vmsg.linear.x = state.velocity[0];
+        vmsg.linear.y = state.velocity[1];
+        vmsg.linear.z = state.velocity[2];
+
+        // Angular velocity
+        vmsg.angular.x = angular_velocity[0];
+        vmsg.angular.y = angular_velocity[1];
+        vmsg.angular.z = angular_velocity[2];
+
+        res.pose = pmsg;
+        res.velocity = vmsg;
+        return true;
+    }
+
+
+
+    
+    bool prediction_ready(std_srvs::Trigger::Request &req,
+                        std_srvs::Trigger::Response &res)
+    {
+        res.success = has_converged;
+        return true;
+    }
+
+    /* Predict uses time until prediction, norm dist is calculated else where
+        Searcher searcher(state.position, state.velocity, state.acceleration);
+
+        Find time until object is at closest point to base of arm
+        float delta_time = searcher.solve();
+    */
+    bool predict(daedalus_msgs::PredictPosition::Request &req,
+                    daedalus_msgs::PredictPosition::Response &res)
+    {
+        // Orientation prediction
+        float t = (float)req.time_until_prediction;
+        Eigen::Quaternionf orientation = ori_predictor->predict(t);
+        res.orientation = quat_to_msg(orientation);
+        
+        // Position prediction
+        Position::State state_prediction;
+        pos_filter->predict_state(t, state_prediction);
+
+        geometry_msgs::Point position_msg;
+        position_msg.x = state_prediction.position(0);
+        position_msg.y = state_prediction.position(1);
+        position_msg.z = state_prediction.position(2);
+        res.position = position_msg;  
+
+        return true;
+    }
+    
+    void run()
+    {
+        Position::StateVector state_guess = Position::StateVector::Zero();
+
+        // Kinda close guess
+        state_guess(0) = -0.2;
+        state_guess(3) = -0.1;
+        state_guess(6) = 0.05;
+
+        // -------------------- Velocity Estimator -----------------------
+        int rate_freq;
+        float acceleration_variance;
+        float measurement_variance;
+        float initial_uncertainty_guess;
+
+        if (nh->hasParam("/rates/trajectory_predictor")) 
         {
-            measured_position = point_to_v3f(msg->position);
-            measured_orientation = quat_to_v4f(msg->orientation);
-
-            if (!position_recvd)
-            {
-                position_recvd = true;
-                init_time = ros::Time::now();
-            }
-
+            nh->getParam("/rates/trajectory_predictor", rate_freq);
+        } else {
+            ROS_ERROR("Rate Parameters not loaded.");
         }
-
-        // Currently only works for position
-        bool observe(daedalus_msgs::ObjectObservation::Request &req,
-                     daedalus_msgs::ObjectObservation::Response &res)
+        if (nh->hasParam("TrajectoryPredictorConfig/acceleration_variance"))
         {
-            Position::State state;
-            pos_filter->get_state(state);
-
-            Eigen::Quaternionf orientation;
-            Eigen::Vector3f angular_velocity;
-            ori_predictor->get_state(orientation, angular_velocity);
-
-            geometry_msgs::Pose pmsg;
-            // Position
-            pmsg.position.x = state.position[0];
-            pmsg.position.y = state.position[1];
-            pmsg.position.z = state.position[2];
-
-            // Orientation
-            pmsg.orientation.w = orientation.w();
-            pmsg.orientation.x = orientation.vec()[0];
-            pmsg.orientation.y = orientation.vec()[1];
-            pmsg.orientation.z = orientation.vec()[2];
-
-            geometry_msgs::Twist vmsg;
-            // Velocity
-            vmsg.linear.x = state.velocity[0];
-            vmsg.linear.y = state.velocity[1];
-            vmsg.linear.z = state.velocity[2];
-
-            // Angular velocity
-            vmsg.angular.x = angular_velocity[0];
-            vmsg.angular.y = angular_velocity[1];
-            vmsg.angular.z = angular_velocity[2];
-
-            res.pose = pmsg;
-            res.velocity = vmsg;
-            return true;
+            nh->getParam("TrajectoryPredictorConfig/acceleration_variance", acceleration_variance);
+            nh->getParam("TrajectoryPredictorConfig/measurement_variance", measurement_variance);
+            nh->getParam("TrajectoryPredictorConfig/initial_uncertainty_guess", initial_uncertainty_guess);
+        } else {
+            ROS_ERROR("TrajectoryPredictorConfig parameters not loaded.");
         }
+        
+        float rate_period = 1.0 / (float)rate_freq;
+        ROS_INFO("rate period: %f", rate_period);
 
+        pos_filter = new PositionFilter(state_guess, initial_uncertainty_guess, rate_period, acceleration_variance, measurement_variance);
+        ori_predictor = new OrientationPredictor(rate_period);
 
+        // ----------------------------------------------------------------
+
+        ros::Rate rate(rate_freq);
 
         
-        bool prediction_ready(std_srvs::Trigger::Request &req,
-                            std_srvs::Trigger::Response &res)
+
+        ros::Time prediction_time;
+        float prediction_duration = 5;
+        Eigen::Matrix<float, 3, 1> prediction;
+
+        while(ros::ok())
         {
-            res.success = has_converged;
-            return true;
-        }
 
-        /* Predict uses time until prediction, norm dist is calculated else where
-            Searcher searcher(state.position, state.velocity, state.acceleration);
-
-            Find time until object is at closest point to base of arm
-            float delta_time = searcher.solve();
-        */
-        bool predict(daedalus_msgs::PredictPosition::Request &req,
-                     daedalus_msgs::PredictPosition::Response &res)
-        {
-            // Orientation prediction
-            float t = (float)req.time_until_prediction;
-            Eigen::Quaternionf orientation = ori_predictor->predict(t);
-            res.orientation = quat_to_msg(orientation);
-            
-            // Position prediction
-            Position::State state_prediction;
-            pos_filter->predict_state(t, state_prediction);
-
-            geometry_msgs::Point position_msg;
-            position_msg.x = state_prediction.position(0);
-            position_msg.y = state_prediction.position(1);
-            position_msg.z = state_prediction.position(2);
-            res.position = position_msg;  
-
-            return true;
-        }
-        
-        void run()
-        {
-            Position::StateVector state_guess = Position::StateVector::Zero();
-
-            // Kinda close guess
-            state_guess(0) = -0.2;
-            state_guess(3) = -0.1;
-            state_guess(6) = 0.05;
-
-            // -------------------- Velocity Estimator -----------------------
-            int rate_freq;
-            float acceleration_variance;
-            float measurement_variance;
-            float initial_uncertainty_guess;
-
-            if (nh->hasParam("/rates/trajectory_predictor")) 
+            if (!has_converged)
             {
-                nh->getParam("/rates/trajectory_predictor", rate_freq);
-            } else {
-                ROS_ERROR("Rate Parameters not loaded.");
-            }
-            if (nh->hasParam("TrajectoryPredictorConfig/acceleration_variance"))
-            {
-                nh->getParam("TrajectoryPredictorConfig/acceleration_variance", acceleration_variance);
-                nh->getParam("TrajectoryPredictorConfig/measurement_variance", measurement_variance);
-                nh->getParam("TrajectoryPredictorConfig/initial_uncertainty_guess", initial_uncertainty_guess);
-            } else {
-                ROS_ERROR("TrajectoryPredictorConfig parameters not loaded.");
-            }
-            
-            float rate_period = 1.0 / (float)rate_freq;
-            ROS_INFO("rate period: %f", rate_period);
-
-            pos_filter = new PositionFilter(state_guess, initial_uncertainty_guess, rate_period, acceleration_variance, measurement_variance);
-            ori_predictor = new OrientationPredictor(rate_period);
-
-            // ----------------------------------------------------------------
-
-            ros::Rate rate(rate_freq);
-
-            
-
-            ros::Time prediction_time;
-            float prediction_duration = 5;
-            Eigen::Matrix<float, 3, 1> prediction;
-
-            while(ros::ok())
-            {
-
-                if (!has_converged)
+                if (position_recvd)
                 {
-                    if (position_recvd)
-                    {
-                        ori_predictor->step(measured_orientation);
-                
-                        // Step prediction
-                        Position::StateVector state_estimate;
-                        pos_filter->step(measured_position);
-                        pos_filter->get_state(state_estimate);
-
-                        // Test for convergence
-                        if (abs(state_estimate(2)) < 0.005)
-                        {
-                            float converge_duration = (ros::Time::now() - init_time).toSec();
-                            ROS_INFO("Converge_time: %f", converge_duration);
-                            has_converged = true;
-                        }
-                        
-                    } else {
-                        ROS_INFO("Waiting for position");
-                    }
-                }
-                else {
-                    
                     ori_predictor->step(measured_orientation);
-
-                    // Step position
+            
+                    // Step prediction
                     Position::StateVector state_estimate;
-
                     pos_filter->step(measured_position);
                     pos_filter->get_state(state_estimate);
 
                     // Test for convergence
-                    if(abs(state_estimate(2)) > 1)
+                    if (abs(state_estimate(2)) < 0.005)
                     {
-                        has_converged = false;
-                        init_time = ros::Time::now();
+                        float converge_duration = (ros::Time::now() - init_time).toSec();
+                        ROS_INFO("Converge_time: %f", converge_duration);
+                        has_converged = true;
                     }
+                    
+                } else {
+                    ROS_INFO("Waiting for position");
                 }
+            }
+            else {
+                
+                ori_predictor->step(measured_orientation);
 
-                ros::spinOnce();
-                rate.sleep();
+                // Step position
+                Position::StateVector state_estimate;
+
+                pos_filter->step(measured_position);
+                pos_filter->get_state(state_estimate);
+
+                // Test for convergence
+                if(abs(state_estimate(2)) > 1)
+                {
+                    has_converged = false;
+                    init_time = ros::Time::now();
+                }
             }
 
-            ros::spin();
+            ros::spinOnce();
+            rate.sleep();
         }
+    }
 
 };
 
